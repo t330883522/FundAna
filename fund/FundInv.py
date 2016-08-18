e4fe94363490f7f9b55d3fe2b766c45bd9b2ff69
JL <jei888@qq.com>
2016-08-18 15:01:15 +0800

    +++

--- fund/FundInv.py
+++ fund/FundInv.py
@@ -1,27 +1,32 @@
 # @Author  : JL
 # @Time    : 2016/8/15 0:28
+from datetime import datetime
+
 from util import MongoUtil
 
 
 class FundInv:
-    pool = MongoUtil.db["pool"]
-
     def __init__(self, code, minInv, startMoney=0, startShare=0):
         self.code = code
         self.minInv = minInv
         self.startMoney = startMoney
         self.startShare = startShare
-        self.__history__ = []
+        self._history = []
 
     def insert(self):
-        global pool
-        pool.insert(self);
+        print(self.__dict__)
+        MongoUtil.pool.delete_one({"code":self.code});
+        MongoUtil.pool.insert(self.__dict__);
+        print(MongoUtil.db.collection_names(include_system_collections=False))
 
     @property
     def history(self):
-        global pool
-        self.__history__ = pool.find()
-        return self.__history__
+        self._history = MongoUtil.pool.find()
+        return self._history
+
+    @history.setter
+    def history(self, value):
+        self._history = value
 
 
 class Inverst:
@@ -33,3 +38,6 @@ class Inverst:
 
 fi = FundInv("162411", 100, startMoney=100, startShare=169.81)
 fi.insert()
+inv=Inverst(datetime.now().date(),150,0.8,50)
+print(inv.__dict__)
+MongoUtil.pool.update({"code":"162411"},{"$push":  {"_history":inv.__dict__}})
